ca5a4bf69869a8a54742d330d2f7543169252df0
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-07 14:20:21 +0100

    93 INCREMENTAL STATIC GENERATION

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import path from 'path';
-import fs from 'fs/promises'; //filesystem module z Nodejs wbudowana bilbioteka
+import path from "path";
+import fs from "fs/promises"; //filesystem module z Nodejs wbudowana bilbioteka
 
 function HomePage(props) {
   // props jest przygotowane przez getStaticProps
@@ -16,18 +16,25 @@ function HomePage(props) {
 
 // prefetch data before we create component. before component is created by nextjs
 export async function getStaticProps() {
+  //tylko gdy [npm run build] ale on nie działa na serwerze naprawdę
+  // WADA PRZESTARZAŁYCH DANYCH gdy mam dane, które się zmieniają ciągle, trzeba ciągle rebuild i redeploy
+  //INCREMENTAL STATIC GENERATION ciągle renderowanie co iles sekund [ZNACZENIE NA PRODUKCJI]
   //sama nazwa mówi - przygotowuje propsy dla twojego komponentu
   // musi zwocić obiekt który ma props atrybut
-  
+
+  console.log('(RE-)Generating...');
+
   // uwaga na ściezki! jak Nectjs uruchamia pliki, to tak jalby wszystkie były w folderze root głównym, a nie /pages jak tutuaj
-  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json') //process.cwd current working director globalnie dostepny obiekt - sciezka od której zaczynamy
-  const jsonData = await fs.readFile(filePath) // asycn zwraca promise
-  const data = JSON.parse(jsonData)
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json"); //process.cwd current working director globalnie dostepny obiekt - sciezka od której zaczynamy
+  const jsonData = await fs.readFile(filePath); // asycn zwraca promise
+  const data = JSON.parse(jsonData);
 
   return {
     props: {
-      products: data.products
+      products: data.products,
     },
+    // INCREMENTAL STATIC GENERATION
+    revalidate: 10, // co 10 sekund; ale serwerze developerskim zawsze się uruhamia, ZNACZENIE NA PRODUKCJI
   };
 }
 

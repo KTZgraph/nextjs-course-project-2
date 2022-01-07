e1410bc62adc09cda0e64ade7765dfa62ed3d1c4
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-07 13:48:14 +0100

    90 getStaticProps() na sztywno

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,25 @@
 function HomePage(props) {
+  // props jest przygotowane przez getStaticProps
+  const { products } = props;
+
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
     </ul>
   );
+}
+
+// prefetch data before we create component. before component is created by nextjs
+export async function getStaticProps() {
+  //sama nazwa mówi - przygotowuje propsy dla twojego komponentu
+  // musi zwocić obiekt który ma props atrybut
+  return {
+    props: {
+      products: [{ id: "p1", title: "Product 1" }], //też musi być obiektem
+    },
+  };
 }
 
 export default HomePage;

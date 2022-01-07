e5b792321d50ee79abbf8739d845fb1ba47058a2
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-07 14:27:07 +0100

    notFound: true

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ function HomePage(props) {
 }
 
 // prefetch data before we create component. before component is created by nextjs
-export async function getStaticProps() {
+export async function getStaticProps(context) {
   //tylko gdy [npm run build] ale on nie działa na serwerze naprawdę
   // WADA PRZESTARZAŁYCH DANYCH gdy mam dane, które się zmieniają ciągle, trzeba ciągle rebuild i redeploy
   //INCREMENTAL STATIC GENERATION ciągle renderowanie co iles sekund [ZNACZENIE NA PRODUKCJI]
@@ -29,12 +29,17 @@ export async function getStaticProps() {
   const jsonData = await fs.readFile(filePath); // asycn zwraca promise
   const data = JSON.parse(jsonData);
 
+  if (data.products.lenght === 0){
+    return {notFound: true};
+  }
+
   return {
     props: {
       products: data.products,
     },
-    // INCREMENTAL STATIC GENERATION
+    // INCREMENTAL STATIC GENERATION ISR przy informacji z npm run build
     revalidate: 10, // co 10 sekund; ale serwerze developerskim zawsze się uruhamia, ZNACZENIE NA PRODUKCJI
+    notFound: true, // jak tru o zwóic 404 azmiast nowej strony; rpzdayne przy błedzie pobierania danych
   };
 }
 
